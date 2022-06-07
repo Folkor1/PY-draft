6b22a7357c7da6deaac4a716bf2f51d81da6b43a
Folkor1 <igorvasiljevs@gmail.com>
2022-06-07 19:08:49 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,36 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+class colors:
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    WHITE = '\033[97m'
+
+def welcome():
+    print("======================================================================")
+    print("Hello and welcome to this very useful matrix determinant finder tool!")
+    print("======================================================================")
+    print(" ")
+    print("                               Enter the")
+    print(colors.GREEN + "              __  __           _            _        ")
+    print(colors.GREEN + "             |  \/  |   __ _  | |_   _ __  (_) __  __")
+    print(colors.GREEN + "             | |\/| |  / _` | | __| | '__| | | \ \/ /")
+    print(colors.GREEN + "             | |  | | | (_| | | |_  | |    | |  >  < ")
+    print(colors.GREEN + "             |_|  |_|  \__,_|  \__| |_|    |_| /_/\_\ ")
+    print(colors.WHITE + " ")
+    print("======================================================================")
+    print("")
+    print("======================================================================")
+    print(" ")
+    print(" ")
+
+def start_menu():
+    """ 
+    Function to select between 2 options,
+    view instructions or login.
+    """
+    
+    
+def main():
+    welcome()
+
+main()
