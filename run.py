48d3acd345f4ab17a334c075282f94fb0602e9cd
Folkor1 <igorvasiljevs@gmail.com>
2022-06-09 23:09:56 +0000

    Experiment

--- run.py
+++ run.py
@@ -22,10 +22,9 @@ def welcome():
     """
     Welcome message.
     """
-    print("======================================================================")
+    print("=" * 70)
     print("Hello and welcome to this very useful matrix determinant finder tool!")
-    print("======================================================================")
-    print(" ")
+    print("=" * 70)
     print("                               Enter the")
     print(colors.GREEN + "              __  __           _            _        ")
     print(colors.GREEN + "             |  \/  |   __ _  | |_   _ __  (_) __  __")
@@ -33,38 +32,42 @@ def welcome():
     print(colors.GREEN + "             | |  | | | (_| | | |_  | |    | |  >  < ")
     print(colors.GREEN + "             |_|  |_|  \__,_|  \__| |_|    |_| /_/\_\ ")
     print(colors.WHITE + " ")
-    print("======================================================================")
+    print("=" * 70)
     print("")
-    print("======================================================================")
+    print("=" * 70)
     print(" ")
     print(" ")
 
-def start_menu():
-    """ 
-    Select between 2 options,
-    view instructions or login.
+def login_user():
     """
-    print('Please select one the the following options:\n')
-    print('1 - Show "How to"')
-    print('2 - Login')
-    main_input()
-
-def main_input():
+    Display the login message and validate the user input.
     """
-    Validate the input on main screen.
-    """
-    first_input = input()
-    while True:     
-        if first_input == "1":
-            clear_console()
-            how_to()
-        elif first_input == "2":
-            clear_console()
-            login_user()
-        else:
-            print(f"\nYou entered: {first_input}. Please enter 1 or 2.")
-            main_input()
-        return
+    login_input = input('Type in the username: ')
+    login = SHEET.worksheet('creds')
+    login_col = login.col_values(1)
+    pass_col = login.col_values(2)
+    while True:
+        try:
+            if login_input not in login_col:
+                print(f'No such user "{login_input}" exists. Please use the correct username.\n')
+                login_user()
+            else:
+                print('Success!\n')
+                login_ind = login_col.index(login_input)
+            pass_input = input('Type in password: ')
+            if pass_input not in pass_col:
+                print('no pass succ1')
+                login_user()
+            else:
+                pass_ind = pass_col.index(pass_input)
+            if pass_ind != login_ind:
+                print('no pass succ2')
+            else:
+                clear_console()
+                print('Login successful!\n\n')
+                how_to()
+        except ValueError():
+            print('error')
 
 def clear_console():
     """
@@ -76,6 +79,8 @@ def how_to():
     """
     Show 'How to' text.
     """
+    print('==========================================================================================================')
+    print('\nHow to:')
     print('\nEnter 2, 3 or 4 numbers, separated by comma.')
     print('\nDepending on which number you have entered, the programm will request next batch of numbers - 2, 3 or 4.')
     print('\nFor example, if you entered 3, then the program will request 3 more numbers 2 more times.')
@@ -85,7 +90,7 @@ def how_to():
     print(colors.GREEN + '   0   8   1')
     print(colors.GREEN + '   9   7   6')
     print(colors.WHITE + '   \nThe matrix determinant is: -201')
-    how_to_continue()
+    print('===========================================================================================================')
 
 def how_to_continue():
     """
@@ -114,26 +119,10 @@ def how_to_validate():
             how_to_validate()
         return
 
-def login_user():
-    """
-    Display the login message and validate the user input.
-    """
-    login_input = input('Enter the username: ')
-    login = SHEET.worksheet('creds')
-    login_col = login.col_values(1)
-    while True:
-        try:
-            if login_input not in login.col_values(1):
-                print(f'No such user "{login_input}" exists. Please enter the correct username.\n')
-                login_user()
-            else:
-                print('succ')
-            return
-        except ValueError():
-            print('error')
+
 
 def main():
     welcome()
-    start_menu()
+    login_user()
 
 main()
