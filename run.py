b40951ced3a09edd35b6addb2295de354ca513d0
Folkor1 <igorvasiljevs@gmail.com>
2022-06-10 17:27:49 +0000

    Exp

--- run.py
+++ run.py
@@ -15,17 +15,17 @@ SHEET = GSPREAD_CLIENT.open('Enter_the_Matrix')
 
 class colors:
     GREEN = '\033[92m'
-    YELLOW = '\033[93m'
     WHITE = '\033[97m'
 
 def welcome():
     """
     Welcome message.
     """
+    print("\n\n")
     print("=" * 70)
     print("Hello and welcome to this very useful matrix determinant finder tool!")
     print("=" * 70)
-    print("                               Enter the")
+    print("\n                               Enter the")
     print(colors.GREEN + "              __  __           _            _        ")
     print(colors.GREEN + "             |  \/  |   __ _  | |_   _ __  (_) __  __")
     print(colors.GREEN + "             | |\/| |  / _` | | __| | '__| | | \ \/ /")
@@ -35,14 +35,13 @@ def welcome():
     print("=" * 70)
     print("")
     print("=" * 70)
-    print(" ")
-    print(" ")
+    login_user()
 
 def login_user():
     """
-    Display the login message and validate the user input.
+    Display the login message and validate the user/password input.
     """
-    login_input = input('Type in the username: ')
+    login_input = input('\nType in the username: ')
     login = SHEET.worksheet('creds')
     login_col = login.col_values(1)
     pass_col = login.col_values(2)
@@ -52,22 +51,21 @@ def login_user():
                 print(f'No such user "{login_input}" exists. Please use the correct username.\n')
                 login_user()
             else:
-                print('Success!\n')
-                login_ind = login_col.index(login_input)
-            pass_input = input('Type in password: ')
-            if pass_input not in pass_col:
-                print('no pass succ1')
-                login_user()
-            else:
-                pass_ind = pass_col.index(pass_input)
-            if pass_ind != login_ind:
-                print('no pass succ2')
-            else:
-                clear_console()
-                print('Login successful!\n\n')
-                how_to()
+                print('Username correct.\n')
+                pass_input = input('Type in password: ')
+                if pass_input not in pass_col:
+                    print('\nIncorrect password. Please try again.\n')
+                    login_user()
+                elif login_col.index(login_input) != pass_col.index(pass_input):
+                    print('\nIncorrect password. Please try again.\n')
+                    login_user()
+                else:
+                    clear_console()
+                    print('\nLogin successful!\n\n')
+                    how_to()
+            return
         except ValueError():
-            print('error')
+            print('Invalid data. Please try again.')
 
 def clear_console():
     """
@@ -79,10 +77,10 @@ def how_to():
     """
     Show 'How to' text.
     """
-    print('==========================================================================================================')
+    print('=' * 90)
     print('\nHow to:')
     print('\nEnter 2, 3 or 4 numbers, separated by comma.')
-    print('\nDepending on which number you have entered, the programm will request next batch of numbers - 2, 3 or 4.')
+    print('\nDepending on your input, the programm will request next batch of numbers - 2, 3 or 4.')
     print('\nFor example, if you entered 3, then the program will request 3 more numbers 2 more times.')
     print('\nThen the program will return the matrix and its determinant.')
     print('\nExample:\n')
@@ -90,39 +88,112 @@ def how_to():
     print(colors.GREEN + '   0   8   1')
     print(colors.GREEN + '   9   7   6')
     print(colors.WHITE + '   \nThe matrix determinant is: -201')
-    print('===========================================================================================================')
+    print('=' * 90)
+    loggen_in_menu()
 
-def how_to_continue():
+def loggen_in_menu():
     """
-    Select between 2 options: log in or return to the start screen.
+    Start calculations or return to main screen.
     """
-    print('\n\nPlease select one the the following options:\n')
-    print('1 - Return to the start screen')
-    print('2 - Login')
-    how_to_validate()
+    print('\n\nSelect one of the following options:')
+    print("\n1 - start")
+    print("2 - return to the main screen")
+    logged_input = input()
+    if logged_input == "1":
+        start()
+    elif logged_input == "2":
+        clear_console()
+        welcome()
+    else:
+        print(f"\nYou entered: {logged_input}. Please enter 1 or 2.")
+        loggen_in_menu()
 
-def how_to_validate():
+def start():
     """
-    Validate the input on 'How to' screen.
+    Accept input and build the matrix.
     """
-    how_to_input = input()
-    while True:
-        if how_to_input == "1":
-            clear_console()
-            welcome()
-            start_menu()
-        elif how_to_input == "2":
-            clear_console()
-            login_user()
+    clear_console()
+    print('\n\nStarting...')
+    print('\nReady!')
+    print('\nEnter 2, 3 or 4 numbers, separated by comma:')
+    matrix_input = input()
+    matrix = []
+    matrix_data = matrix_input.split(',')
+    if validate_first_input(matrix_data):
+        matrix.append(matrix_input)
+        while True:
+            if len(matrix_data) == 2:
+                print('\nEnter another 2 numbers, separated by comma:')
+                row_2_2_input = input()
+                row_2_2_data = row_2_2_input.split(',')
+                if validate_row_2_2(row_2_2_data):
+                    matrix.append(row_2_2_input)
+                    return matrix
+            elif len(matrix_data) == 3:
+                print('\nEnter another 3 numbers:')
+                row_2_3_input = input()
+                row_2_3_data = row_2_3_input.split(',')
+                if validate_row_2_3(row_2_3_data):
+                    matrix.append(row_2_3_input)
+                    while True:
+                        print('\nEnter final 3 numbers:')
+                        row_3_3_input = input()
+                        row_3_3_data = row_3_3_input.split(',')
+                        if validate_row_2_3(row_3_3_data):
+                            matrix.append(row_3_3_input)
+                            return matrix
+
+def validate_first_input(values):
+    """
+    Validate the first input, if 2, 3 or 4 numbers are entered.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) not in range(2, 5):
+            print(f"\nNeed to enter 2, 3 or 4 numbers. You entered: {len(values)}")
         else:
-            print(f"\nYou entered: {how_to_input}. Please enter 1 or 2.")
-            how_to_validate()
-        return
+            return True
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.")
+
+def validate_row_2_2(values):
+    """
+    Validate the second input with 2 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 2:
+            print(f"\nNeed to enter 2 numbers. You entered: {len(values)}")
+        else:
+            return True
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.")
+
+def validate_row_2_3(values):
+    """
+    Validate the second input with 3 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 3:
+            print(f"\nNeed to enter 3 numbers. You entered: {len(values)}")
+        else:
+            return True
+    except ValueError as e:
+        print(f"Invalid data: {e}. Please try again.")
+        return False
 
 
+res = start()
+print(res)
 
-def main():
-    welcome()
-    login_user()
 
-main()
+class Matrix:
+    """
+    Matrix class.
+    """
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+
+# welcome()
